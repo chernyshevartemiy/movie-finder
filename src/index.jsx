b39695d94f7c272bcb1408107f9af34622bd541b
Artem Chernyshev <artem.octopus@mail.ru>
2022-12-15 21:49:14 +0300

    hash router

--- src/index.jsx
+++ src/index.jsx
@@ -2,17 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import store from "./redux/store";
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
+import store from './redux/store';
+import { Provider } from 'react-redux';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log('Work');
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
-      <App/>
+      <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
