a9dd173126ba79493923fc93e8a8140ae89cfc89
Artem Chernyshev <artem.octopus@mail.ru>
2022-12-15 20:49:27 +0300

    app is ready

--- src/redux/async/fetchMovies.js
+++ src/redux/async/fetchMovies.js
@@ -1,26 +1,26 @@
-import {createAsyncThunk} from "@reduxjs/toolkit";
-import {setMovies} from "../movieSlice";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { setMovies } from '../movieSlice';
 
 export const fetchMovies = createAsyncThunk(
   'movie/fetchMovies',
-  async (query, {
-    rejectWithValue,
-    dispatch,
-  }) => {
+  async (query, { rejectWithValue, dispatch }) => {
     try {
       if (query) {
-        const response = await fetch(`https://search.imdbot.workers.dev?q=${query}`)
-        const data = await response.json()
+        const response = await fetch(
+          `https://search.imdbot.workers.dev?q=${query}`
+        );
+        const data = await response.json();
         if (data.description.length === 0 || data.ok !== true) {
-          return rejectWithValue('error')
+          return rejectWithValue('error');
         } else {
-          dispatch(setMovies(data.description))
+          dispatch(setMovies(data.description));
         }
       } else if (query === '') {
-        dispatch(setMovies([]))
-        return rejectWithValue(null)
+        dispatch(setMovies([]));
+        return rejectWithValue(null);
       }
     } catch (error) {
-      return rejectWithValue('error')
+      return rejectWithValue('error');
     }
-  })
+  }
+);
--- src/redux/movieSlice.js
+++ src/redux/movieSlice.js
@@ -1,48 +1,48 @@
-import {createSlice} from "@reduxjs/toolkit";
-import {getRecentMoviesFromLs} from "../utils/getRecentMoviesFromLs";
-import {fetchMovies} from "./async/fetchMovies";
+import { createSlice } from '@reduxjs/toolkit';
+import { getRecentMoviesFromLs } from '../utils/getRecentMoviesFromLs';
+import { fetchMovies } from './async/fetchMovies';
 
 const initialState = {
   movies: [],
-  status:      null,
+  status: null,
   recentMovies: getRecentMoviesFromLs(),
-}
+};
 const movieSlice = createSlice({
   name: 'movie',
   initialState,
   reducers: {
     setMovies: (state, action) => {
-      state.movies = action.payload
+      state.movies = action.payload;
     },
     setRecentMovies: (state, action) => {
-      state.recentMovies.unshift(action.payload)
-      const uniqueId = []
+      state.recentMovies.unshift(action.payload);
+      const uniqueId = [];
       const uniqueMovies = state.recentMovies.filter((movie) => {
-        const isDuplicate = uniqueId.includes(movie['#IMDB_ID'])
+        const isDuplicate = uniqueId.includes(movie['#IMDB_ID']);
         if (!isDuplicate) {
-          uniqueId.push(movie['#IMDB_ID'])
-          return true
+          uniqueId.push(movie['#IMDB_ID']);
+          return true;
         }
-        return false
-      })
-      state.recentMovies = uniqueMovies
-      if (state.recentMovies.length > 7 ) {
-        state.recentMovies = state.recentMovies.slice(0, 7)
+        return false;
+      });
+      state.recentMovies = uniqueMovies;
+      if (state.recentMovies.length > 7) {
+        state.recentMovies = state.recentMovies.slice(0, 7);
       }
-      localStorage.setItem('recentMovies', JSON.stringify(state.recentMovies))
-    }
+      localStorage.setItem('recentMovies', JSON.stringify(state.recentMovies));
+    },
   },
   extraReducers: (builder) => {
     builder.addCase(fetchMovies.fulfilled, (state, action) => {
-      state.status = 'success'
+      state.status = 'success';
     });
     builder.addCase(fetchMovies.pending, (state, action) => {
-      state.status = 'loading'
+      state.status = 'loading';
     });
     builder.addCase(fetchMovies.rejected, (state, action) => {
-      state.status = action.payload
-    })
-  }
-})
-export const {setMovies, setRecentMovies} = movieSlice.actions
-export default movieSlice.reducer
+      state.status = action.payload;
+    });
+  },
+});
+export const { setMovies, setRecentMovies } = movieSlice.actions;
+export default movieSlice.reducer;
