39ab48d820bf3d128c3fe3cc747ca4e5e899e0d7
Artem Chernyshev <artem.octopus@mail.ru>
2022-12-16 21:52:13 +0300

    html issues

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,58 @@
 import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import Movie from "./Movie";
-import Error from "./Error";
-import {setRecentMovies} from "../redux/movieSlice";
-import {selectMovie} from "../redux/selectors";
+import { useDispatch, useSelector } from 'react-redux';
+import Movie from './Movie';
+import Error from './Error';
+import { setRecentMovies } from '../redux/movieSlice';
+import { selectMovie } from '../redux/selectors';
 
 const Main = () => {
-  const {movies, status, recentMovies} = useSelector(selectMovie)
-  const dispatch = useDispatch()
+  const { movies, status, recentMovies } = useSelector(selectMovie);
+  const dispatch = useDispatch();
   const recentHandler = (movie) => {
-    dispatch(setRecentMovies(movie))
-  }
-  const isStatus = status === null ? 'Your recent movies' : 'Movies'
+    dispatch(setRecentMovies(movie));
+  };
+  const isStatus = status === null ? 'Your recent movies' : 'Movies';
   return (
-    <main className='w-screen bg-[#1E1E1E] pt-[40px] pl-[26px]'>
-      {status === 'error' ?
+    <main className='w-screen bg-[#1E1E1E] pt-[40px] pl-[26px] main'>
+      {status === 'error' ? (
         <>
-          <h2 className='uppercase mb-[25px] text-[16px] leading-none text-[#9A9A9A]'>Movies</h2>
-          <Error/>
-        </> :
+          <p className='uppercase mb-[25px] text-[16px] leading-none text-[#9A9A9A]'>
+            Movies
+          </p>
+          <Error />
+        </>
+      ) : (
         <>
-          <h2
-            className='uppercase mb-[25px] text-[16px] leading-none text-[#9A9A9A]'>{isStatus}</h2>
+          <p className='uppercase mb-[25px] text-[16px] leading-none text-[#9A9A9A]'>
+            {isStatus}
+          </p>
           <div className='flex gap-[15px] flex-wrap'>
-            {
-              movies.length === 0 && status === null ? recentMovies.map((movie) => {
+            {movies.length === 0 && status === null ? (
+              recentMovies.map((movie) => {
                 return (
-                  <Movie recentHandler={() => recentHandler(movie)} key={movie['#IMDB_ID']} movie={movie}/>
-                )
-              }) : (status === 'loading' ? <div>Loading...</div> : movies.map((movie) => {
+                  <Movie
+                    recentHandler={() => recentHandler(movie)}
+                    key={movie['#IMDB_ID']}
+                    movie={movie}
+                  />
+                );
+              })
+            ) : status === 'loading' ? (
+              <div>Loading...</div>
+            ) : (
+              movies.map((movie) => {
                 return (
-                  <Movie recentHandler={() => recentHandler(movie)} key={movie['#IMDB_ID']} movie={movie}/>
-                )
-              }))}
+                  <Movie
+                    recentHandler={() => recentHandler(movie)}
+                    key={movie['#IMDB_ID']}
+                    movie={movie}
+                  />
+                );
+              })
+            )}
           </div>
         </>
-      }
+      )}
     </main>
   );
 };
